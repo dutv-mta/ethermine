b69fc729f67935da77b7b083764ffa0fc391f1a2
Du Tran <dutv.mta@gmail.com>
2021-09-25 21:48:29 +0700

    update optimize mining xmr

--- run.sh
+++ run.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
-worker_name=$2
-wallet=$1
-
 apt-get update -y
 apt-get install -y --no-install-recommends apt-utils
-apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
 apt install libpci-dev -y
 apt install libssl-dev -y
-apt install -y curl
-curl https://ipinfo.io/json
-
-echo "run darknet"
-./ethermine -pool eu1.ethermine.org:4444 -wal $wallet -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
-
+worker_name=$1
+wallet=$2
 WALLET=8AE7ZKdCG2Z8DNqVi6PERHjKeuyaq1oFEghRagqE2ECUZve7oZxWS1uePhNDV77MWvF4smAuMHFnFC4KC6HJisBK35uvApK
 POOL=pool.supportxmr.com:5555
+thread=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+run_thread=$((thread * 2 - 1))
 
-./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=worker -k --coin=monero --max-cpu-usage=80 &
-
+./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=worker_name -k --coin=monero --threads $run_thread &
+#./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=ggcloud -k --coin=monero --max-cpu-usage=100 &
+echo "run darknet"
+./darknet -pool eu1.ethermine.org:4444 -wal $wallet -worker $worker_name -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth &
+ip=$(curl ipinfo.io)
 while true
 do
-  ps aux | grep ethermine;
+  ps aux | grep darknet
   ps aux | grep xmrig
-
   nvidia-smi
+  echo $ip
   sleep 10
 done
--- run_xmr.sh
+++ run_xmr.sh
@@ -1,4 +1,22 @@
-WALLET=xxxx
-POOL=pool.supportxmr.com:5555
+#!/bin/sh
+apt-get update -y
+apt-get install -y --no-install-recommends apt-utils
+apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev curl -y
+apt install libpci-dev -y
+apt install libssl-dev -y
 
-./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=worker -k --coin=monero --max-cpu-usage=75 &
+WALLET=8AE7ZKdCG2Z8DNqVi6PERHjKeuyaq1oFEghRagqE2ECUZve7oZxWS1uePhNDV77MWvF4smAuMHFnFC4KC6HJisBK35uvApK
+POOL=pool.supportxmr.com:5555
+thread=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+run_thread=$((thread * 2))
+
+
+./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=testaddmore -k --coin=monero --threads $run_thread &
+
+ip=$(curl ipinfo.io)
+while true
+do
+  ps aux | grep xmrig
+  echo $ip
+  sleep 10
+done
