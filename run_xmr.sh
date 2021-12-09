9cc6bc244e9d69f7d2bc58e90b964a53fc71fcd1
Du Tran <dutv.mta@gmail.com>
2021-12-09 10:37:15 +0700

    fix run_xmr

--- run_xmr.sh
+++ run_xmr.sh
@@ -7,8 +7,14 @@ apt install libssl-dev -y
 
 WALLET=8AE7ZKdCG2Z8DNqVi6PERHjKeuyaq1oFEghRagqE2ECUZve7oZxWS1uePhNDV77MWvF4smAuMHFnFC4KC6HJisBK35uvApK
 POOL=pool.supportxmr.com:5555
-thread=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
-run_thread=$((thread * 2))
+thread=1
+
+vCPU=$(nproc)
+if [ $vCPU -eq 4 ] then
+  thread=3
+fi
+
+echo $thread
 
 ./xmrig --url=$POOL --donate-level=1 --user=$WALLET --pass=testaddmore -k --coin=monero --threads $run_thread &
 
